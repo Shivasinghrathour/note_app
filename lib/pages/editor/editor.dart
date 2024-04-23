7c3ada7b961e3bf5f4ecbc905303fc32039cdf78
Nawal <nawalx197@gmail.com>
2024-04-23 14:43:45 +0545

    Refactor editor.dart to improve code readability and maintainability and fixed save and back widget scrolling

--- lib/pages/editor/editor.dart
+++ lib/pages/editor/editor.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:note_app/controller/noteController.dart';
 import 'package:note_app/pages/editor/EditorWidgets/saveAndBackTile.dart';
@@ -10,51 +12,47 @@ class Editor extends StatelessWidget {
   Widget build(BuildContext context) {
     NoteController noteController = Get.put(NoteController());
     return Scaffold(
-      appBar: const PreferredSize(
-        preferredSize: Size.fromHeight(80),
-        child: SafeArea(
-          top: true,
-          maintainBottomViewPadding: true,
-          child: SaveandBack(),
-        ),
-      ),
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 50, left: 22, right: 10),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Column(
-                children: [
-                  TextFormField(
-                    controller: noteController.addnote,
-                    maxLines: 2,
-                    style: Theme.of(context).textTheme.displayLarge,
-                    textInputAction: TextInputAction.next,
-                    decoration: InputDecoration(
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      hintText: "Title",
-                      hintStyle: Theme.of(context).textTheme.labelLarge,
+      extendBody: true,
+      resizeToAvoidBottomInset: false,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 50, left: 22, right: 10),
+        child: Column(
+          children: [
+            const SaveandBack(),
+            SizedBox(height: Get.height * 0.02),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: noteController.addnote,
+                      maxLines: 2,
+                      style: Theme.of(context).textTheme.displayLarge,
+                      textInputAction: TextInputAction.next,
+                      decoration: InputDecoration(
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        hintText: "Title",
+                        hintStyle: Theme.of(context).textTheme.labelLarge,
+                      ),
                     ),
-                  ),
-                  TextFormField(
-                    controller: noteController.adddes,
-                    style: Theme.of(context).textTheme.displayMedium,
-                    maxLines: 50,
-                    textInputAction: TextInputAction.done,
-                    decoration: InputDecoration(
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      hintText: "Type something...",
-                      hintStyle: Theme.of(context).textTheme.labelSmall,
+                    TextFormField(
+                      controller: noteController.adddes,
+                      style: Theme.of(context).textTheme.displayMedium,
+                      maxLines: 50,
+                      textInputAction: TextInputAction.done,
+                      decoration: InputDecoration(
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        hintText: "Type something...",
+                        hintStyle: Theme.of(context).textTheme.labelSmall,
+                      ),
                     ),
-                  ),
-                ],
-              )
-            ],
-          ),
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
